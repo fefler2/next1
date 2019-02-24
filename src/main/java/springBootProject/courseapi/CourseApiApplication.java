e19b8dc83c7aa9f109325b917c8f9dec1e79c85e
giciarz412 <tomaszfefler1990@gmail.com>
2019-02-24 17:53:15 +0100

    1

---
+++ src/main/java/springBootProject/courseapi/CourseApiApplication.java
@@ -0,0 +1,21 @@
+package springBootProject.courseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApplication {
+
+
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CourseApiApplication.class, args);
+
+        TopicController topicController = new TopicController();
+        topicController.getAllTopics();
+
+    }
+
+}
