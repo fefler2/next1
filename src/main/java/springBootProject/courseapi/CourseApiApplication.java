0f281eb6d3d16c0716f8bd868785d90c0cd0c41b
giciarz412 <tomaszfefler1990@gmail.com>
2019-02-27 17:53:37 +0100

    1-1

--- src/main/java/springBootProject/courseapi/CourseApiApplication.java
+++ src/main/java/springBootProject/courseapi/CourseApiApplication.java
@@ -7,19 +7,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseApiApplication {
 
 
+    private static boolean ifPalindrom(String s) {
+        // s -> s2, ? s.equals(s2)
 
+        String s2 = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            s2 += s.charAt(i);
+        }
+
+        return s.equals(s2);
+    }
 
     public static void main(String[] args) {
 
         SpringApplication.run(CourseApiApplication.class, args);
 
+        String s = "acad";
+        System.out.println(ifPalindrom(s));
+
         TopicController topicController = new TopicController();
         topicController.getAllTopics();
-
-
-
-
-
 
 
     }
---
+++ src/main/java/springBootProject/courseapi/Singleton.java
@@ -0,0 +1,20 @@
+package springBootProject.courseapi;
+
+public class Singleton {
+    private static Singleton singleton;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getSingleton(){
+        if (singleton == null){
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
